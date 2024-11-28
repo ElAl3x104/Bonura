6ea405ba8ccc756a2a9cf13145dacf986323dde5
ElAl3x104 ! <bonura.alex@iisgalvanimi.edu.it>
2024-11-28 08:13:35 +0000

    primo commit

---
+++ bonura/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SecondComponentComponent, ThirdComponentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'bonura';
+}
---
+++ bonura/src/app/second-component/second-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FourthComponentComponent } from '../fourth-component/fourth-component.component';
+
+@Component({
+  selector: 'app-second-component',
+  imports: [],
+  templateUrl: './second-component.component.html',
+  styleUrl: './second-component.component.css'
+})
+export class SecondComponentComponent {
+
+}
